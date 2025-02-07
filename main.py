4fb258af8162acd36d2d0493998da410f8cf6c0c
T_yasutomo <gon235711@gmail.com>
2025-02-07 23:31:34 +0900

    フォントを変数で管理。button6が正常に削除されるように

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from tkinter import messagebox
 import random
 import time
 
-#色関係
+#色、フォント
 labels_bg = "gray100"
 mw_bg = "white"
+nomal_font = ("Helvetica", 20)
+big_font = ("Helvetica", 30)
 
 # メインウィンドウの設定
 root = tk.Tk()
@@ -53,43 +55,40 @@ def choose(num):
     global state, mode, selected_question, text, selected_mode
     #print(state)
     if state == 0:
-        button6.grid_forget
         selected_mode = num
         state = 1
         choice_generation()
         print("state0->1")
         #print(text)
     elif state == 1:
-        button6.grid_forget
         state = 2
         print("state1->2")
         #print(text[num],vocabularies[selected_question]["meaning"])
         if text[num] == vocabularies[selected_question]["meaning"]:
-            tochuu(0)
+            judgement(0)
             print("OK!")
             #playsound('C:\\Users\\gon23\\OneDrive\\デスクトップ\\python\\english\\goodsound.mp3')
         else:
-            tochuu(1)
+            judgement(1)
         #print(text)
     elif state == 2:
         if num == 6:
-            button6.grid_forget
+            button6.grid_forget()
             state = 1
+            print("state2->1")
             selected_question = random.randint(0, n_rows - 1)
             choice_generation()
         elif text[num] == vocabularies[selected_question]["meaning"]:
-            tochuu(0)
+            judgement(0)
             print("OK!")
-            #playsound('C:\\Users\\gon23\\OneDrive\\デスクトップ\\python\\english\\goodsound.mp3')
         else:
-            tochuu(1)
-   
+            judgement(1)
     update_buttons()
     #print("aa")
     #time.sleep(1)
-    label0.config(fg="black")
+    #label0.config(fg="black")
 
-def tochuu(num):
+def judgement(num):
     if num == 0:
         button6.config(text="OK!")
     elif num ==1:
@@ -123,33 +122,31 @@ def update_buttons():
     label0.config(text=text[6])
 
 
-label0 = tk.Label(root, text=text[6], width=15, height=2, bg=labels_bg, font=("Helvetica", 30), relief="solid")
+label0 = tk.Label(root, text=text[6], width=15, height=2, bg=labels_bg, font=big_font, relief="solid")
 label0.grid(row=2, column=1, columnspan=3)
-#label1 = tk.Label(root, text="", width=15, height=1, bg=labels_bg, font=("Helvetica", 30), relief="solid")
-#label1.grid(row=3, column=1, columnspan=3)
-button0 = tk.Button(root, text=text[0], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(0))
+button0 = tk.Button(root, text=text[0], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(0))
 button0.grid(row=4, column=1)
-button1 = tk.Button(root, text=text[1], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(1))
+button1 = tk.Button(root, text=text[1], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(1))
 button1.grid(row=4, column=3)
-button2 = tk.Button(root, text=text[2], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(2))
+button2 = tk.Button(root, text=text[2], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(2))
 button2.grid(row=6, column=1)
-button3 = tk.Button(root, text=text[3], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(3))
+button3 = tk.Button(root, text=text[3], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(3))
 button3.grid(row=6, column=3)
-button4 = tk.Button(root, text=text[4], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(4))
+button4 = tk.Button(root, text=text[4], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(4))
 button4.grid(row=8, column=1)
-button5 = tk.Button(root, text=text[5], width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(5))
+button5 = tk.Button(root, text=text[5], width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(5))
 button5.grid(row=8, column=3)
-button6 = tk.Button(root, text="", width=25, bg=labels_bg, font=("Helvetica", 20), relief="solid", command=lambda: choose(6))
+button6 = tk.Button(root, text="", width=25, bg=labels_bg, font=nomal_font, relief="solid", command=lambda: choose(6))
 
 
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=0, column=2)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=0, column=2)
 
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=0, column=0)
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=1, column=0)
-tk.Label(root, text="", width=9, height=3 ,bg=mw_bg, font=("Helvetica", 20)).grid(row=3, column=0)
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=5, column=0)
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=7, column=0)
-tk.Label(root, text="", width=9, bg=mw_bg, font=("Helvetica", 20)).grid(row=9, column=0)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=0, column=0)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=1, column=0)
+tk.Label(root, text="", width=9, height=3 ,bg=mw_bg, font=nomal_font).grid(row=3, column=0)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=5, column=0)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=7, column=0)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font).grid(row=9, column=0)
 
 
 # プログラム起動時に本を読み込む
