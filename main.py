dfc93bffde9a9ddf05376657a07cb16f6d1c7235
T_yasutomo <gon235711@gmail.com>
2025-02-16 20:11:33 +0900

    ラジオボタンの設置

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ labels_bg = "gray100"   #ラベルの色
 mw_bg = "white" #背景の色
 nomal_font = ("Helvetica", 20)
 big_font = ("Helvetica", 30)
+small_font = ("Helvetica", 10)
 bw = 3  #ラベル、ボタンの枠線の太さ
 my_file = "words.csv"
 
@@ -65,12 +66,13 @@ def choose(num):
             judgement(1)
 
 def next():
-    global state,selected_question
+    global state,selected_question,check
     button6.grid_forget()
     buttons[answer_num].config(fg="black")
     state = 1
     choice_generation()
     update_buttons()
+    check=op0
 
 def go_to_home():
     global state,selected_question
@@ -87,7 +89,7 @@ def go_to_home():
 def judgement(num):
     buttons[answer_num].config(fg="red")
     button6.config(text= "true" if num==0 else "false")
-    button6.grid(row=10,column=1,columnspan=3)
+    button6.grid(row=11,column=1,columnspan=5,rowspan=3)
 
 def choice_generation():
     global selected_vocabularies,selected_question,answer_num
@@ -125,33 +127,45 @@ filter = [vocabularies,filtered_verb,filtered_noun,filtered_adjective,filtered_a
 
 #問を出すラベル
 label0 = tk.Label(root, text=text[6], width=15, height=2, bg=labels_bg, font=big_font, relief="solid", borderwidth=bw)
-label0.grid(row=2, column=1, columnspan=3)
+label0.grid(row=2, column=2, columnspan=3, rowspan=2)
 #選択肢用ボタン
 button0 = tk.Button(root, text=text[0], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(0))
-button0.grid(row=4, column=1)
+button0.grid(row=5, column=1, columnspan=2)
 button1 = tk.Button(root, text=text[1], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(1))
-button1.grid(row=4, column=3)
+button1.grid(row=5, column=4, columnspan=2)
 button2 = tk.Button(root, text=text[2], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(2))
-button2.grid(row=6, column=1)
+button2.grid(row=7, column=1, columnspan=2)
 button3 = tk.Button(root, text=text[3], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(3))
-button3.grid(row=6, column=3)
+button3.grid(row=7, column=4, columnspan=2)
 button4 = tk.Button(root, text=text[4], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(4))
-button4.grid(row=8, column=1)
+button4.grid(row=9, column=1, columnspan=2)
 button5 = tk.Button(root, text=text[5], width=30, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: choose(5))
-button5.grid(row=8, column=3)
+button5.grid(row=9, column=4, columnspan=2)
 #次へ行くボタン
 button6 = tk.Button(root, text="", width=25, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: next())
 #ホームボタン
 button7 = tk.Button(root, image=photo2, width=50,height=50, bg=labels_bg, font=nomal_font, relief="solid", borderwidth=bw, command=lambda: go_to_home())
 buttons = [button0,button1,button2,button3,button4,button5]
 
+check = tk.StringVar(value=0)
+op0 = tk.Radiobutton(root, text= "OK!", bg=labels_bg, font=small_font, width=4,relief="solid", variable=check, value=0)
+op1 = tk.Radiobutton(root, text= "umm…", bg=labels_bg, font=small_font, width=4, relief="solid", variable=check, value=1)
+op0.grid(row=11,column=0)
+op1.grid(row=12,column=0)
+
+#button6.grid(row=11,column=1,columnspan=5,rowspan=2)
+
+#button6.grid_forget()
+
 #レイアウト調整用の空白ラベル
-tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font, height=2).grid(row=0, column=2)
+tk.Label(root, text="", width=9, bg=mw_bg, font=nomal_font, height=2).grid(row=0, column=3)
 tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=1, column=0)
-tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font, height=2).grid(row=3, column=0)
-tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=5, column=0)
-tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=7, column=0)
-tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=9, column=0)
+tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font, height=2).grid(row=4, column=0)
+tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=6, column=0)
+tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=8, column=0)
+tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=10, column=0)
+
+tk.Label(root, text="", width=4, bg=mw_bg, font=nomal_font).grid(row=13, column=0)
 
 
 # メインループ
